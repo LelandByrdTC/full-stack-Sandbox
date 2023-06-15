b0fc8297982b4c1bafef95980db7eacadd6eaaee
Leland Byrd <lbyrd@truecoders.io>
2023-06-15 14:23:12 -0500

    created config for mysql2 with dotenv

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express() // this is an instance of the express method
+const PORT = 3001
 
 //MIDDLEWARE
 app.use(express.json())
 
 app.get("/", (request, response)=>{
-    response.write("<h1> Home for SuperHero App HOORAY!</h1>")
     response.status(200)
+    response.write("<h1> Home for SuperHero App HOORAY!</h1>")
     response.end()
 })
 
-app.listen(3001, ()=> {
-    console.log("server is listening at port 3001")
+app.get("/heroes", (req, res)=>{
+    res.status(200)
+    res.write("<h2> Batman </h2>")
+    res.end()
+})
+
+app.post("/heroes", (req, res)=>{
+    res.status(201)
+    const responseBody = {
+        body: req.body,
+        url: req.url,
+        method: req.method 
+    }
+    res.json(responseBody)
+    res.end()
+})
+
+app.listen(PORT, ()=> {
+    console.log(`server is listening at port ${PORT}`)
 })
